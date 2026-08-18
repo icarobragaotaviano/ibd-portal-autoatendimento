e4cc33d1503617acf4989c1335678c7304da1f7f
Icaro Braga <icarobragaotaviano@gmail.com>
2026-08-18 00:05:00 -0300

    refactor: remove legacy calendar index

--- lib/calendar/index.ts
+++
@@ -1,7 +0,0 @@
-import { calendarMode } from "@/lib/config";
-import { googleCalendarProvider } from "@/lib/calendar/google";
-import { mockCalendarProvider } from "@/lib/calendar/mock";
-
-export function getCalendarProvider() {
-  return calendarMode() === "google" ? googleCalendarProvider : mockCalendarProvider;
-}
