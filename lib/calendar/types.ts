210e5b632d8ac9c49b0f160f634795dc7a015124
Icaro Braga <icarobragaotaviano@gmail.com>
2026-08-18 00:05:08 -0300

    refactor: remove legacy calendar types

--- lib/calendar/types.ts
+++
@@ -1,6 +0,0 @@
-import type { BookingInput, BookingResult, BusyInterval } from "@/lib/types";
-
-export interface CalendarProvider {
-  getBusyIntervals(dateISO: string): Promise<BusyInterval[]>;
-  createBooking(input: BookingInput, endISO: string): Promise<BookingResult>;
-}
